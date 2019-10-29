a1e06fe26a4b5d7d479035081ff76eca7966977b
SebaStad <SebaStad@users.noreply.github.com>
2019-10-29 16:13:05 +0100

    Adjustment to plot functions
    Added plot and print methods, $plot_area can now be called without arguments

---
+++ R/pint_palm_ncdf_berlin.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param obj Call print function of R6 palm_ncdf class
+#'
+#' @return Some Strings
+#' @export
+#'
+#' @examples
+print.palm_ncdf_berlin <- function(obj){
+  obj$plot()
+}
---
+++ R/plot_palm_ncdf_berlin.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param obj A R6 plalm_ncdf Class
+#'
+#' @return A ggplot
+#' @export
+#'
+#' @examples
+plot.palm_ncdf_berlin <- function(obj){
+  obj$plot_area()
+}
